254da900f885464d9842753fb0e185949888bd96
godofbeauty <2375029420@qq.com>
2015-09-25 15:52:34 +0800

    GOB20150925155231

---
+++ Queue/StaticQueue.h
@@ -0,0 +1,23 @@
+/*		Author: jianzhedeng		*/
+/*		Date: 2015.9.25		*/
+/*		Def: ¾²Ì¬Õ»¡£ÒÑÍ¨¹ý°×ºÐ²âÊÔ¡£		*/
+#pragma once
+#ifndef STATICQUEUE_H_
+#define STATICQUEUE_H_
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+#define MAXSIZE (1024)
+#define OK (0)
+#define ERROR (1)
+typedef int StackElemType;
+typedef int status;
+
+StackElemType *InitStack(int *StackLength);
+StackElemType *ReleaseStack(StackElemType *Stack, int *StackLength);
+StackElemType Pop(StackElemType *Stack, int *StackLength);
+status Push(StackElemType *Stack, int *StackLength, StackElemType ele);
+
+void ShowStack(StackElemType *Stack, int *StackLength);
+
+#endif
