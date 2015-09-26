1991fa7af59cb3af2b5eb6f2500704359936e1e4
jianzhedeng <2928450849@qq.com>
2015-09-26 17:24:09 +0800

    jzd20150926172400

---
+++ SingleLinkedList/SingleLinkedList_Linked_cpp.h
@@ -0,0 +1,14 @@
+/*		Author: jianzhedeng		*/
+/*		Date: 2015.8.25		*/
+/*		Def: 用指针实现的简单的单向链表。这个类将用纯粹的C++实现，以供被更复杂的结构继承或调用。		*/
+#pragma  once
+#ifndef SINGLELINKEDLIST_LINKED_CPP
+#define SINGLELINKEDLIST_LINKED_CPP
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+#define OK (0)
+#define ERROR (1)
+typedef int status;
+
+#endif
