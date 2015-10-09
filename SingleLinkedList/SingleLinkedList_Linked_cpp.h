a579e286b856a72a7d7d18264f458fc54c1836d3
godofbeauty <2375029420@qq.com>
2015-10-09 13:21:51 +0800

    GOB20151009132146

--- SingleLinkedList/SingleLinkedList_Linked_cpp.h
+++ SingleLinkedList/SingleLinkedList_Linked_cpp.h
@@ -42,8 +42,8 @@ protected:
 			return (p);
 		}
 		return (NULL);
-
 	}
+
 public:
 	class Node
 	{
@@ -53,47 +53,10 @@ public:
 		Node() :data(0), next(NULL){;}
 		Node(T ele) :data(ele), next(NULL){;}
 	};
-	SingleLinkedList()
-	{
-		InitList();
-	}
-	~SingleLinkedList()
-	{
-		ReleaseList();
-	}
-	status Insert(Node &node, const int pos = 0)
-	{
-		/*	检查pos是否大于节点数量		*/
-		/*	pos为零则插入最后		*/
-		/*	pos不为零则插入相应节点之前		*/
-		int i = 0;
-		Node *p = NULL;
-		if (pos > 0 && pos <= GetLength())
-		{
-			for (i = 0, p = this->Head; i < (pos - 1); ++i, p = p->next)
-			{
-			}
-			node.next = p->next;
-			p->next = &node;
-			return (OK);
-		}
-		else if (pos == 0)
-		{
-			for (i = 0, p = this->Head; p->next != NULL; ++i, p = p->next)
-			{
-			}
-			node.next = p->next;
-			p->next = &node;
-			return (OK);
-		}
-		return (ERROR);
-	}
-	status Insert(const T ele, const int pos = 0)
-	{
-		/*	此处应使用new关键字来申请结点空间		*/
-		Node *p = new Node(ele);
-		return (Insert(*p, pos));
-	}
+	SingleLinkedList();
+	~SingleLinkedList();
+	status Insert(Node &node, const int pos = 0);
+	status Insert(const T ele, const int pos = 0);
 	Node *Take(const int pos = 0)
 	{
 		int i = 0;
@@ -120,70 +83,11 @@ public:
 		}
 		return (p);
 	}
-	status Delete(const int pos = 0)
-	{
-		Node *p = NULL;
-		p = this->Take(pos);
-		if (NULL != p)
-		{
-			/*	适时释放		*/
-			return (OK);
-		} 
-		else
-		{
-			return (ERROR);
-		}
-	}
-	T Get(const int pos = 0)
-	{
-		int i = 0;
-		int num = GetLength();
-		Node *p = NULL;
-		if (pos > 0 && pos < num)
-		{
-			for (i = 0, p = this->Head; i < pos; ++i, p = p->next)
-			{
-			}
-			return (p->data);
-		}
-		else if (pos == 0 || pos == num)
-		{
-			for (i = 0, p = this->Head; i < num; ++i, p = p->next)
-			{
-			}
-			return (p->data);
-		}
-		return (NULL);
-	}
-	status Set(const T ele, const int pos = 0)
-	{
-		Node *p = NULL;
-		p = this->GetNode(pos);
-		if (NULL != p)
-		{
-			p->data = ele;
-			return (OK);
-		}
-		return (ERROR);
-	}
-	void Print()
-	{
-		Node *p = NULL;
-		int i = 0, len = GetLength();
-		cout << "This is a SingleLinkedList, with " << len << " nodes." << endl;
-		for (i = 0, p = this->Head; p->next != NULL; ++i, p = p->next)
-		{
-			cout << p->next->data;
-			if (i < len - 1)
-			{
-				cout << "\t";
-			} 
-			else if (i == len - 1)
-			{
-				cout << endl;
-			}
-		}
-	}
+
+	status Delete(const int pos = 0);
+	T Get(const int pos = 0);
+	status Set(const T ele, const int pos = 0);
+	void Print();
 };
 
 template <class T>
@@ -223,6 +127,125 @@ int SingleLinkedList<T>::GetLength()
 	return (i);
 }
 
+template <class T>
+SingleLinkedList<T>::SingleLinkedList()
+{
+	InitList();
+}
 
+template <class T>
+SingleLinkedList<T>::~SingleLinkedList()
+{
+	ReleaseList();
+}
+
+template <class T>
+status SingleLinkedList<T>::Insert(Node &node, const int pos)
+{
+	/*	检查pos是否大于节点数量		*/
+	/*	pos为零则插入最后		*/
+	/*	pos不为零则插入相应节点之前		*/
+	int i = 0;
+	Node *p = NULL;
+	if (pos > 0 && pos <= GetLength())
+	{
+		for (i = 0, p = this->Head; i < (pos - 1); ++i, p = p->next)
+		{
+		}
+		node.next = p->next;
+		p->next = &node;
+		return (OK);
+	}
+	else if (pos == 0)
+	{
+		for (i = 0, p = this->Head; p->next != NULL; ++i, p = p->next)
+		{
+		}
+		node.next = p->next;
+		p->next = &node;
+		return (OK);
+	}
+	return (ERROR);
+}
+
+template <class T>
+status SingleLinkedList<T>::Insert(const T ele, const int pos)
+{
+	/*	此处应使用new关键字来申请结点空间		*/
+	Node *p = new Node(ele);
+	return (Insert(*p, pos));
+}
+
+template <class T>
+status SingleLinkedList<T>::Delete(const int pos)
+{
+	Node *p = NULL;
+	p = this->Take(pos);
+	if (NULL != p)
+	{
+		/*	适时释放		*/
+		return (OK);
+	}
+	else
+	{
+		return (ERROR);
+	}
+}
+
+template <class T>
+T SingleLinkedList<T>::Get(const int pos)
+{
+	int i = 0;
+	int num = GetLength();
+	Node *p = NULL;
+	if (pos > 0 && pos < num)
+	{
+		for (i = 0, p = this->Head; i < pos; ++i, p = p->next)
+		{
+		}
+		return (p->data);
+	}
+	else if (pos == 0 || pos == num)
+	{
+		for (i = 0, p = this->Head; i < num; ++i, p = p->next)
+		{
+		}
+		return (p->data);
+	}
+	return (NULL);
+}
+
+template <class T>
+status SingleLinkedList<T>::Set(const T ele, const int pos)
+{
+	Node *p = NULL;
+	p = this->GetNode(pos);
+	if (NULL != p)
+	{
+		p->data = ele;
+		return (OK);
+	}
+	return (ERROR);
+}
+
+template <class T>
+void SingleLinkedList<T>::Print()
+{
+	Node *p = NULL;
+	int i = 0, len = GetLength();
+	cout << "This is a SingleLinkedList, with " << len << " nodes." << endl;
+	for (i = 0, p = this->Head; p->next != NULL; ++i, p = p->next)
+	{
+		cout << p->next->data;
+		if (i < len - 1)
+		{
+			cout << "\t";
+		}
+		else if (i == len - 1)
+		{
+			cout << endl;
+		}
+	}
+}
 
 #endif
